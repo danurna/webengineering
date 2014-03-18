d6a6f22a7745442591eee6417902cd20328f9cda
Florian Groh <flo.groh@gmail.com>
2014-03-19 00:16:48 +0100

    auto-indentation

--- UE1/Umsetzung/js/framework.js
+++ UE1/Umsetzung/js/framework.js
@@ -1,18 +1,18 @@
 /*
-    Helper functions for the first exercise of the Web Engineering course
+Helper functions for the first exercise of the Web Engineering course
 */
 
-/* 
-    checks if native form validation is available.
-    Source/Further informations: http://diveintohtml5.info/everything.html
+/*
+checks if native form validation is available.
+Source/Further informations: http://diveintohtml5.info/everything.html
 */
 function hasFormValidation() {
     return 'noValidate' in document.createElement('form');
 }
 
-/* 
-    checks if native date input is available.
-    Source/Further informations: http://diveintohtml5.info/everything.html
+/*
+checks if native date input is available.
+Source/Further informations: http://diveintohtml5.info/everything.html
 */
 function hasNativeDateInput(){
     var i = document.createElement('input');
@@ -23,8 +23,8 @@ function hasNativeDateInput(){
 var DATE_DELIMITERS = ['/','\\','-'];
 
 /*
-    returns the string representation of a date input field in the format dd.mm.yyyy.
-    If the value of the input field can't be interpreted as a date, the original value is returned.
+returns the string representation of a date input field in the format dd.mm.yyyy.
+If the value of the input field can't be interpreted as a date, the original value is returned.
 */
 function getNormalizedDateString(selector){
     value = $(selector).val();
@@ -39,12 +39,12 @@ function getNormalizedDateString(selector){
 }
 
 /*
-    returns the string representation of the given value (seconds) in the format mm:ss.
+returns the string representation of the given value (seconds) in the format mm:ss.
 */
 function secToMMSS(value){
     var minutes = Math.floor(value / 60);
     var seconds = (value % 60);
-    
+
     if(seconds < 10){
         seconds = "0" + seconds;
     }
