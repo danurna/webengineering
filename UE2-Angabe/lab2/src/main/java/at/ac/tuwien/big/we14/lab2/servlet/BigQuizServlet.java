e5871925e9309ee726b6591f8848119c47cb23bd
Daniel Witurna <daniel.witurna@gmail.com>
2014-04-14 15:45:50 +0200

    Added Servlet test page and big quiz servlet implementation like in vorlesungsfolien ;)

--- UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,3 +1,25 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
-// Your Servlet implementation
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name ="BigQuiz", urlPatterns = { "/BigQuizServlet" })
+public class BigQuizServlet extends HttpServlet{
+    private static final long serialVersionUID = -2708561549069343716L;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+        {
+            resp.setContentType("text/html);charset=UTF-8");
+            PrintWriter out = resp.getWriter();
+            out.println("<html><body><h1>Hello "+ req.getParameter("userName") +"! Big Quiz Serlvet here!</h1></body></html>");
+        }
+    
+}
