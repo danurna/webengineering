d9be21efecd04c1fa6be6eda145862ee23f23d8a
Daniel Witurna <daniel.witurna@gmail.com>
2014-04-24 16:06:03 +0200

    player impl

--- UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/SimplePlayer.java
+++ UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/api/impl/SimplePlayer.java
@@ -3,44 +3,64 @@ package at.ac.tuwien.big.we14.lab2.api.impl;
 import at.ac.tuwien.big.we14.lab2.api.Player;
 
 public class SimplePlayer implements Player {
-	private String name;
+    private String name;
     // Stores true, false or null (if unknown).
     private Boolean[] roundAnswers;
+    // Stores after which amount of time the user answered a question.
+    private Integer[] roundAnswerTimes;
     private int wonRounds;
 
     public SimplePlayer() {
-    	roundAnswers = new Boolean[3];
+        roundAnswers = new Boolean[3];
     }
-    
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public void setRoundAnswer(int index, boolean correct) {
-    	roundAnswers[index] = correct;
+        roundAnswers[index] = correct;
     }
-    
+
     public Boolean getRoundAnswer(int index) {
-    	return roundAnswers[index];
+        return roundAnswers[index];
     }
-    
+
     public Boolean[] getRoundAnswers() {
-		return roundAnswers;
-	}
-	
+        return roundAnswers;
+    }
+
     public void setRoundAnswers(Boolean[] roundAnswers) {
-		this.roundAnswers = roundAnswers;
-	}
-	
+        this.roundAnswers = roundAnswers;
+    }
+
     public int getWonRounds() {
-		return wonRounds;
-	}
-	
+        return wonRounds;
+    }
+
     public void setWonRounds(int wonRounds) {
-		this.wonRounds = wonRounds;
-	}
+        this.wonRounds = wonRounds;
+    }
+
+    public Integer[] getRoundAnswerTimes() {
+        return roundAnswerTimes;
+    }
+
+    public void setRoundAnswerTimes(Integer[] roundAnswerTimes) {
+        this.roundAnswerTimes = roundAnswerTimes;
+    }
+
+    public void setRoundAnswerTime(int index, int time) {
+        roundAnswerTimes[index] = time;
+    }
+
+    public int getRoundAnswerTime(int index) {
+        return roundAnswerTimes[index];
+    }
+
+    
 }
