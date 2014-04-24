fa249d43a3294c6307795b6280646d4eddcde91f
Florian Groh <flo.groh@gmail.com>
2014-04-24 17:00:56 +0200

    servlet grundgerüst

--- UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -17,45 +17,57 @@ import at.ac.tuwien.big.we14.lab2.api.QuestionDataProvider;
 import at.ac.tuwien.big.we14.lab2.api.QuizFactory;
 import at.ac.tuwien.big.we14.lab2.api.impl.ServletQuizFactory;
 
+@WebServlet(name = "BigQuiz", urlPatterns = { "/BigQuizServlet" })
+public class BigQuizServlet extends HttpServlet {
+	private static final long serialVersionUID = -2708561549069343716L;
+	final static int maxRounds = 5;
+	final static int maxQuestions = 3;
 
-@WebServlet(name ="BigQuiz", urlPatterns = { "/BigQuizServlet" })
-public class BigQuizServlet extends HttpServlet{
-    private static final long serialVersionUID = -2708561549069343716L;
-    final static int maxRounds = 5;
-    final static int maxQuestions = 3; 
-    
-    
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException
-        {
-            if( request.getParameter("action").equals("start") ){
-                response.setContentType("text/html;charset=UTF-8");
-                PrintWriter out = response.getWriter();
-                out.println("<html><body><h1>Start! Big Quiz Servlet here!</h1></body></html>");     
-            }
-            
-        }
-    
-    private void printList(){
-        ServletContext servletContext = getServletContext();
-        QuizFactory factory = ServletQuizFactory.init(servletContext); 
-        QuestionDataProvider provider = factory.createQuestionDataProvider();
-        List<Category> categories = provider.loadCategoryData();
-        for( Iterator<Category> it = categories.iterator(); it.hasNext(); ){
-            System.out.println(it.next());
-        }   
-    }
-    
-    private void startNewGame(){
-        
-    }
-    
-    private void showNextQuestions(){
-        
-    }
-    
-    private void showNextRound(){
-        
-    }
-    
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String actionParam = request.getParameter("action");
+		if(actionParam == null) { return; } 
+		
+		switch (actionParam) {
+		case "start":
+			startNewGame(request, response);
+			break;
+
+		case "nextQuestion":
+			showNextQuestion(request, response);
+			break;
+
+		case "nextRound":
+			showNextRound(request, response);
+			break;
+
+		default:
+			return;
+		}
+
+	}
+
+	private void printList() {
+		ServletContext servletContext = getServletContext();
+		QuizFactory factory = ServletQuizFactory.init(servletContext);
+		QuestionDataProvider provider = factory.createQuestionDataProvider();
+		List<Category> categories = provider.loadCategoryData();
+		for (Iterator<Category> it = categories.iterator(); it.hasNext();) {
+			System.out.println(it.next());
+		}
+	}
+
+	private void startNewGame(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+	private void showNextQuestion(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+
+	private void showNextRound(HttpServletRequest request, HttpServletResponse response) {
+
+	}
+
 }
