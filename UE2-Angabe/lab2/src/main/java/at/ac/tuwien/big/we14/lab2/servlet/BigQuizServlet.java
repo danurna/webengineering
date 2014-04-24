cc25784f8fe4d7fb73aa6bfbb3b67fc38021311f
Florian Groh <flo.groh@gmail.com>
2014-04-24 18:06:49 +0200

    basic servlet shits

--- UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2-Angabe/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -1,21 +1,27 @@
 package at.ac.tuwien.big.we14.lab2.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import at.ac.tuwien.big.we14.lab2.api.Category;
+import at.ac.tuwien.big.we14.lab2.api.Game;
+import at.ac.tuwien.big.we14.lab2.api.Player;
 import at.ac.tuwien.big.we14.lab2.api.QuestionDataProvider;
 import at.ac.tuwien.big.we14.lab2.api.QuizFactory;
 import at.ac.tuwien.big.we14.lab2.api.impl.ServletQuizFactory;
+import at.ac.tuwien.big.we14.lab2.api.impl.SimpleGame;
+import at.ac.tuwien.big.we14.lab2.api.impl.SimplePlayer;
 
 @WebServlet(name = "BigQuiz", urlPatterns = { "/BigQuizServlet" })
 public class BigQuizServlet extends HttpServlet {
@@ -27,8 +33,10 @@ public class BigQuizServlet extends HttpServlet {
 			HttpServletResponse response) throws ServletException, IOException {
 
 		String actionParam = request.getParameter("action");
-		if(actionParam == null) { return; } 
-		
+		if (actionParam == null) {
+			return;
+		}
+
 		switch (actionParam) {
 		case "start":
 			startNewGame(request, response);
@@ -58,15 +66,37 @@ public class BigQuizServlet extends HttpServlet {
 		}
 	}
 
-	private void startNewGame(HttpServletRequest request, HttpServletResponse response) {
+	private void startNewGame(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession(true);
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatcher = context.getRequestDispatcher("/question.jsp");
 		
+		Player test = new SimplePlayer();
+		test.setName("Test");
+		Player computer = new SimplePlayer();
+		computer.setName("Computer");
+		List<Player> players = new ArrayList<Player>();
+		players.add(test);
+		players.add(computer);
+		
+		
+		Game game = new SimpleGame();
+		game.setPlayers(players);
+		
+		
+		
+		
+		dispatcher.forward(request, response);
 	}
 
-	private void showNextQuestion(HttpServletRequest request, HttpServletResponse response) {
+	private void showNextQuestion(HttpServletRequest request,
+			HttpServletResponse response) {
 
 	}
 
-	private void showNextRound(HttpServletRequest request, HttpServletResponse response) {
+	private void showNextRound(HttpServletRequest request,
+			HttpServletResponse response) {
 
 	}
 
