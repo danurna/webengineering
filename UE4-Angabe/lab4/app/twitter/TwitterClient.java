ad183b3fa57c4708ed7cb884e79f63009751d239
Daniel Witurna <daniel.witurna@digitalsunray.com>
2015-02-11 11:09:38 +0100

    Removed twitter credentials

--- UE4-Angabe/lab4/app/twitter/TwitterClient.java
+++ UE4-Angabe/lab4/app/twitter/TwitterClient.java
@@ -26,10 +26,10 @@ public class TwitterClient implements ITwitterClient{
 	public void publishUuid(TwitterStatusMessage message) throws Exception {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
 		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey("GZ6tiy1XyB9W0P4xEJudQ")
-		  .setOAuthConsumerSecret("gaJDlW0vf7en46JwHAOkZsTHvtAiZ3QUd2mD1x26J9w")
-		  .setOAuthAccessToken("1366513208-MutXEbBMAVOwrbFmZtj1r4Ih2vcoHGHE2207002")
-		  .setOAuthAccessTokenSecret("RMPWOePlus3xtURWRVnv1TgrjTyK7Zk33evp4KKyA");
+		  .setOAuthConsumerKey("")
+		  .setOAuthConsumerSecret("")
+		  .setOAuthAccessToken("")
+		  .setOAuthAccessTokenSecret("");
 		TwitterFactory tf = new TwitterFactory(cb.build());
 		Twitter twitter = tf.getInstance();
 	    Status status = twitter.updateStatus(message.getTwitterPublicationString());
